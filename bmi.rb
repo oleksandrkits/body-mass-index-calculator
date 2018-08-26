8babfbd10b619ccce094defd099255823c62ab34
kitsera <sashakitsera16358@gmail.com>
2018-08-26 21:17:41 +0300

    #3 Add calculation for standart US units.

--- bmi.rb
+++ bmi.rb
@@ -2,7 +2,8 @@ class BMI
 
   attr_reader :bmi
 
-  def initialize(height, weight)
-    @bmi = Float(weight) / Float(height ** 2)
-  end
+  def initialize(height, weight, units)
+    units_multipliar = 1 if units == 1 else 703
+    @bmi = (Float(weight) / Float(height ** 2) * units_multipliar).round 2
+  end  
 end
--- input_handler.rb
+++ input_handler.rb
@@ -1,14 +1,17 @@
 class InputHandler
 
-  attr_reader :height, :weight
+  attr_reader :height, :weight, :units
 
   def initialize
+    puts "Choose your unit system. (1) Metric; (2) Standart US"
+    @units = $stdin.gets.chomp.to_i
+
     puts "Input your height and weight..."
 
-    print 'Height: '
+    print "Height(#{@units == 1? 'm': 'in'}): "
     @height = $stdin.gets.chomp.to_f
 
-    print 'Weight: '
+    print "Weight(#{@units == 1? 'kg': 'lb'}): "
     @weight = $stdin.gets.chomp.to_f
 
     puts "So, you are #{height} tall and your weight is #{weight}"
