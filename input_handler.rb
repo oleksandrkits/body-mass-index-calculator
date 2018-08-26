3ad2f0dcc18e102bafd64f092654785b3186213d
kitsera <sashakitsera16358@gmail.com>
2018-08-26 20:39:59 +0300

    #2 Input handler file added.

---
+++ input_handler.rb
@@ -0,0 +1,9 @@
+puts "Input your height and weight..."
+
+print 'Height: '
+height = $stdin.gets.chomp
+
+print 'Weight: '
+weight = $stdin.gets.chomp
+
+puts "So, you are #{height} tall and your weight is #{weight}"
