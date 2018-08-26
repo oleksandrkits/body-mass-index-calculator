313aa44a7876e2e935049f540f63eb352348807a
kitsera <sashakitsera16358@gmail.com>
2018-08-26 20:48:08 +0300

    #2 Code clearance commit.

--- bmi.rb
+++ bmi.rb
@@ -2,9 +2,7 @@ class BMI
 
   attr_reader :bmi
 
-  def initialize(meter, kg)
-    @meter = Float(meter)
-    @kg = kg
-    @bmi = @kg / (@meter ** 2)
+  def initialize(height, weight)
+    @bmi = Float(weight) / Float(height ** 2)
   end
 end
