7fa54bf96d1268cca02b68b4d835119c17fd1bb5
kitsera <sashakitsera16358@gmail.com>
2018-08-26 21:35:19 +0300

    Explanation for bmi added.

--- application.rb
+++ application.rb
@@ -4,4 +4,4 @@ require_relative "input_handler"
 input = InputHandler.new
 bmi = BMI.new(input.height, input.weight, input.units)
 
-puts "Your body mass index is #{bmi.bmi}"
+puts "Your body mass index is #{bmi.bmi}, wich means #{bmi.explanation}"
--- bmi.rb
+++ bmi.rb
@@ -5,5 +5,25 @@ class BMI
   def initialize(height, weight, units)
     units_multipliar = 1 if units == 1 else 703
     @bmi = (Float(weight) / Float(height ** 2) * units_multipliar).round 2
-  end  
+  end
+
+  def explanation()
+    if @bmi < 15
+      'very severely underweight'
+    elsif (15 <= @bmi) and (@bmi < 16)
+      'severely underweight'
+    elsif (16 <= @bmi) and (@bmi < 18.5)
+      'underweight'
+    elsif (18.5 <= @bmi) and (@bmi < 25)
+      'normal (healthy weight)'
+    elsif (25 <= @bmi) and (@bmi < 30)
+      'overweight'
+    elsif (30 <= @bmi) and (@bmi < 35)
+      'moderately obese'
+    elsif (35 <= @bmi) and (@bmi < 40)
+      'severely obese'
+    else
+      'very severely obese'
+    end
+  end
 end
